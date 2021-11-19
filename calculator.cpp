1dbdab1985b536691cc25642cc29032274e35e3a
Enkk17 <86929986+Enkk17@users.noreply.github.com>
2021-11-19 21:22:12 +0100

    Create calculator.cpp

---
+++ calculator.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    double a,b,total;  
+    char operator_;  
+    cout<<"enter the number ";
+    cin>>a;
+    cout<<"enter the second number ";
+    cin>>b;
+    cout<<"select the operator +,-,,/ ";
+    cin>>operator_;
+    switch (operator_)
+    {
+    case '+': total=a+b;
+    break;
+    case '-': total=a-b;
+    break;
+    case '*': total=a*b;
+    break;
+    case '/': total=a/b;
+    break;
+    if(operator_!='+' && '-' && '*' && '/')
+    {
+    cout<<"the operator entered was not recognized, try again.";
+    }
+    }
+cout<<"the result is "<<total<<endl;;
+return 0;
+}
